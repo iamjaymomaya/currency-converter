19f31d779250462db794b55ca7b60e69cf27beb2
Jay Momaya <jk07momaya@gmail.com>
2023-05-10 00:06:01 +0530

    chore: Created Model, Migration & Seeder for Currency

---
+++ database/seeders/CurrencySeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Feature\CurrencyConversion\Domain\Models\Currency;
+use Carbon\Carbon;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CurrencySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'name' => 'Indian Ruppee',
+                'symbol' => 'INR',
+                'created_at' => Carbon::now()
+            ],
+            [
+                'name' => 'Euro',
+                'symbol' => 'EUR',
+                'created_at' => Carbon::now()
+            ]
+        ];
+
+        Currency::insert($data);
+    }
+}
