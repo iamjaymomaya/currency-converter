2be7258d8475470239c855cf5dc25ede32182120
Jay Momaya <jk07momaya@gmail.com>
2023-05-10 01:52:51 +0530

    chore: Controller methods for rendering & conversion

--- app/Feature/CurrencyConversion/v1/Http/Controllers/CurrencyConversionController.php
+++ app/Feature/CurrencyConversion/v1/Http/Controllers/CurrencyConversionController.php
@@ -2,12 +2,21 @@
 
 namespace App\Feature\CurrencyConversion\v1\Http\Controllers;
 
+use App\Feature\CurrencyConversion\Domain\Models\Currency;
+use App\Feature\CurrencyConversion\Service\Converters\FixerCurrencyConverter;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Client\Request;
 
 class CurrencyConversionController extends Controller
 {
     public function index() 
     {
-        return view('currency-conversion.index');
+        $currencies = Currency::all();
+        return view('currency-conversion.index', compact('currencies'));
+    }
+
+    public function convert(Request $request) {
+        $converter = new FixerCurrencyConverter();
+        return $converter->convert($request->amount, $request->from, $request->to);
     }
 }
