fe1772d459b726b95cf5f104721707fc4ae29d22
Jay Momaya <jk07momaya@gmail.com>
2023-05-10 11:27:32 +0530

    fix: added login & register routes

--- resources/views/layouts/common/partials/navbar.blade.php
+++ resources/views/layouts/common/partials/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">{{env('APP_NAME')}}</a>
+        <a class="navbar-brand" href="/">{{env('APP_NAME')}}</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -12,10 +12,10 @@
                 </li>
                 @else
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Login</a>
+                    <a class="nav-link" href="{{route('login.show')}}">Login</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Register</a>
+                    <a class="nav-link" href="{{route('register.show')}}">Register</a>
                 </li>
                 @endif
             </ul>      
